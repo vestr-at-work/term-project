b89c7ab8d9f626edb060a25c87445e48ff4a5dde
vestr-at-work <petera-david@seznam.cz>
2022-02-06 18:27:02 +0100

    errors corrected and comments added

--- dithering.py
+++ dithering.py
@@ -1,16 +1,27 @@
-from PIL import Image
-import random as rn
+"""
+dithering
+david petera, 1. year of studies
+winter term 2021/22
+NPRG030 – programming 1
+"""
+
+#importing necessary libraries
+from PIL import Image     #image handeling
+import random as rn     #generation of pseudo-random values 
 
 
 def threshold(img): 
+    #loading pixels of the image and size
     pixelMap = img.load()
     width, height = img.size[0], img.size[1]
     print("\ngetting ready for threshold dithering...")
 
+    #going through every pixel and changing its value according to the set threshold
+    threshold = 127
     for j in range(height):
         for i in range(width):
             value = int(pixelMap[i, j])
-            if value > 127:
+            if value > threshold:
                 pixelMap[i, j] = 255
             else:
                 pixelMap[i, j] = 0
@@ -20,10 +31,12 @@ def threshold(img):
 
 
 def random(img):
+    #loading pixels of the image and size
     pixelMap = img.load()
     width, height = img.size[0], img.size[1]
     print("\ngetting ready for random dithering...")
 
+    #going through every pixel and changing its value according to the random threshold
     for j in range(height):
         for i in range(width):
             value = int(pixelMap[i, j])
@@ -37,46 +50,55 @@ def random(img):
 
 
 def clustered_dot(img, mod=1):
+    #loading the right dithering matrix (according to the input) from the image files 
     if mod == 1:
-        dither_arr = Image.open("files/dither_arrays/8x8_vert.png")
+        dither_matrix = Image.open("files/dither_matricies/8x8_vert.png")
         print("\ngetting ready for clustered dot dithering (vertical/horizontal matrix)...")
     if mod == 2:
-        dither_arr = Image.open("files/dither_arrays/8x8_45deg.png")
+        dither_matrix = Image.open("files/dither_matricies/8x8_45deg.png")
         print("\ngetting ready for clustered dot dithering (diagonal matrix)...")
     if mod == 3:
-        dither_arr = Image.open("files/dither_arrays/4x4_bayer.png")
+        dither_matrix = Image.open("files/dither_matricies/4x4_bayer.png")
         print("\ngetting ready for clustered dot dithering (bayer matrix)...")
 
-    dither_arr = dither_arr.convert('L')
-    arr_pixelMap = dither_arr.load()
-    dith_arr_size = dither_arr.size[0]
+    dither_matrix = dither_matrix.convert('L')
+    matrix_pixelMap = dither_matrix.load()
+    dith_matrix_size = dither_matrix.size[0]
 
+    #loading pixels of the image and size
     pixelMap = img.load()
     width, height = img.size[0], img.size[1]
 
+    #going through every pixel and adding its value with the value in the dither matrix and changing its value according to the threshold
+    threshold = 255
     for j in range(height):
         for i in range(width):
-            value = int(pixelMap[i, j]) + int(arr_pixelMap[i%dith_arr_size, j%dith_arr_size])
-            if value > 255:
+            value = int(pixelMap[i, j]) + int(matrix_pixelMap[i%dith_matrix_size, j%dith_matrix_size])
+            if value > threshold:
                 pixelMap[i, j] = 255
             else:
                 pixelMap[i, j] = 0
 
-    dither_arr.close()
+    dither_matrix.close()
     print("..done")
     
     
 
 def error_dif(img, mod=1):
+    #loading pixels of the image and size
     pixelMap = img.load()
     width, height = img.size[0], img.size[1]
 
-    if mod==1:
+    if mod==1:      #if floyd-steinberg selected
+
         print("\ngetting ready for floyd-steinberg error diffusion dithering...")
+
+        #going through every pixel, changing its value according to the threshold and distributing the difference (error) to the neighbouring pixels
+        threshold = 127
         for j in range(height): 
             for i in range(width):
                 value = pixelMap[i, j]
-                if int(value) >= 127:
+                if int(value) >= threshold:
                     dif = value - 255
                     pixelMap[i, j] = 255
                 else:
@@ -92,12 +114,16 @@ def error_dif(img, mod=1):
                 if i+1 < width and j+1 < height:
                     pixelMap[i+1, j+1] += int((1/16)*dif)
     
-    if mod==2:
+    if mod==2:      #if sierra-lite selected
+
         print("\ngetting ready for sierra-lite error diffusion dithering...")
+
+        #going through every pixel, changing its value according to the threshold and distributing the difference (error) to the neighbouring pixels
+        threshold = 127
         for j in range(height):
             for i in range(width):    
                 value = pixelMap[i, j]
-                if int(value) >= 127:
+                if int(value) >= threshold:
                     dif = value - 255
                     pixelMap[i, j] = 255
                 else:
@@ -107,7 +133,7 @@ def error_dif(img, mod=1):
                 if i+1 < width:
                     pixelMap[i+1, j] += int((2/4)*dif)
                 if j+1 < height:
-                    pixelMap[i, j] += int((1/4)*dif)
+                    pixelMap[i, j+1] += int((1/4)*dif)
                 if i-1 > 0 and j+1 < height:
                     pixelMap[i-1, j+1] += int((1/4)*dif)
 
@@ -139,8 +165,8 @@ try:
     dith_type = input("\nenter the number: ")
     input_error = False     #setting default value for input error handling to False
 
-    dith_type = dith_type.strip()
-    dith_type = dith_type.strip(".")
+    dith_type = dith_type.strip()       #striping the input of unwanted leading and trailing space characters
+    dith_type = dith_type.strip(".")    #striping the input of unwanted leading and trailing dot characters
 
     if dith_type == '1':
         threshold(img_gray)
@@ -161,7 +187,7 @@ try:
         input_error = True      
     
     if input_error is not True:
-        img_gray.save("files/output.png")
+        img_gray.save("files/outputs/output.png")       #saving the image
         img_gray.show()     #showing the final image to the user
 
     img_gray.close()
